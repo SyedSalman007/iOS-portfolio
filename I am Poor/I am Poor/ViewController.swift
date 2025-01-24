aab015ed09b63262ee5168f4f726070dc8ddac79
Syed Salman Ali Zaidi <97502374+SyedSalman007@users.noreply.github.com>
2025-01-24 10:44:19 +0500

    Add files via upload

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Salman Zaidi on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
